84e4d60ba382b9790bfcd45e2724e9576f1a11d8
Héctor Hugo Hidalgo Martínez <hhugohm@gmail.com>
2015-11-12 14:12:12 -0600

    formato a la clase

--- spring-ws-client/src/main/java/prueba/TestCountry.java
+++ spring-ws-client/src/main/java/prueba/TestCountry.java
@@ -9,29 +9,26 @@ import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class TestCountry {
 
+	private static WebServiceTemplate webServiceTemplate;
+	private final static String WSDL = "http://localhost:8080/spring-ws/endpoints/ws-services.wsdl";
+	private final static String COUNTRY_SERVICE = "http://localhost:8080/spring-ws/endpoints/getCountryRequest";
 
-    private static  WebServiceTemplate webServiceTemplate;
-    private final static String WSDL="http://localhost:8080/spring-ws/endpoints/ws-services.wsdl";
-    private final static String COUNTRY_SERVICE="http://localhost:8080/spring-ws/endpoints/getCountryRequest";
-    
 	public static void main(String[] args) {
-		
+
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("ws.xml");
-		
-		GetCountryRequest  request = new GetCountryRequest();
+
+		GetCountryRequest request = new GetCountryRequest();
 		request.setName("Spain");
-		
+
 		webServiceTemplate = (WebServiceTemplate) ctx.getBean("webServiceTemplate");
-		
-		
-		GetCountryResponse response = (GetCountryResponse) webServiceTemplate.marshalSendAndReceive(
-				WSDL,
-				request,
+
+		GetCountryResponse response = (GetCountryResponse) webServiceTemplate.marshalSendAndReceive(WSDL, request,
 				new SoapActionCallback(COUNTRY_SERVICE));
 		Country country = response.getCountry();
-		
-		System.out.println(country.getName() +"-"+country.getCapital()+ "-"+ country.getPopulation()+"-"+ country.getCurrency() );
-		
+
+		System.out.println(country.getName() + "-" + country.getCapital() + "-" + country.getPopulation() + "-"
+				+ country.getCurrency());
+
 		ctx.close();
 
 	}
