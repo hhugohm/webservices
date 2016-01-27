2e05e5a73ffc9a001a331631240090a924c51052
Héctor Hugo Hidalgo Martínez <hhugohm@gmail.com>
2016-01-27 10:51:14 -0600

    cambios para funcionamientop de log

--- cxf-rest-ws/src/main/java/org/neos/cxf/rest/service/CxfRestServiceImpl.java
+++ cxf-rest-ws/src/main/java/org/neos/cxf/rest/service/CxfRestServiceImpl.java
@@ -2,26 +2,28 @@ package org.neos.cxf.rest.service;
 
 import javax.ws.rs.core.Response;
 
+import lombok.extern.slf4j.Slf4j;
 import org.neos.cxf.rest.dao.EmployeeDao;
 import org.neos.cxf.rest.ws.CxfRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class CxfRestServiceImpl implements CxfRestService {
-	@Autowired
-	private EmployeeDao employeeDao;
+    @Autowired
+    private EmployeeDao employeeDao;
 
-	@Override
-	public Response getEmployeeDetail(String employeeId) {
-		System.out.println("ENTRANDO AL SERVICIO EMPLEADO REST");
-		if (employeeId == null) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-		// return Response.status(Response.Status.OK).entity(users).build();
-		// return
-		// Response.ok.(employeeDao.getEmployeeDetails(employeeId)).build();
+    @Override
+    public Response getEmployeeDetail(String employeeId) {
+        log.info("ENTRANDO AL SERVICIO EMPLEADO REST-CAMBIO");
+        if (employeeId == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        // return Response.status(Response.Status.OK).entity(users).build();
+        // return
+        // Response.ok.(employeeDao.getEmployeeDetails(employeeId)).build();
 
-		return Response.status(Response.Status.OK).entity(employeeDao.getEmployeeDetails(employeeId)).build();
-	}
+        return Response.status(Response.Status.OK).entity(employeeDao.getEmployeeDetails(employeeId)).build();
+    }
 }
--- cxf-rest-ws/src/main/java/org/neos/cxf/rest/service/UserRestServiceImpl.java
+++ cxf-rest-ws/src/main/java/org/neos/cxf/rest/service/UserRestServiceImpl.java
@@ -2,16 +2,18 @@ package org.neos.cxf.rest.service;
 
 import javax.ws.rs.core.Response;
 
+import lombok.extern.slf4j.Slf4j;
 import org.neos.cxf.rest.model.User;
 import org.neos.cxf.rest.ws.IUserRestService;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserRestServiceImpl implements IUserRestService{
 
 	@Override
 	public Response getUserDetail(String userId) {
-		System.out.println("ENTRANDO AL SERVICIO + ");
+		log.info("ENTRANDO AL SERVICIO - ");
 		
 		User user = new User("HUGO","HIDALGO","hhugohm@gmail.comm");
 		
