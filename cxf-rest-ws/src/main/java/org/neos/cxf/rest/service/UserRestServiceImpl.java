4bd52bf680fca76e7f945e5055d9dbace5333aae
Héctor Hugo Hidalgo Martínez <hhugohm@gmail.com>
2016-01-28 12:56:00 -0600

    se elimina import no necesario

--- cxf-rest-ws/src/main/java/org/neos/cxf/rest/service/UserRestServiceImpl.java
+++ cxf-rest-ws/src/main/java/org/neos/cxf/rest/service/UserRestServiceImpl.java
@@ -1,7 +1,6 @@
 package org.neos.cxf.rest.service;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
